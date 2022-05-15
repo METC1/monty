4dc713994cd13b3301539470054a9681857f0e6a
METC <metc_ck1@hotmail.com>
2022-05-15 10:13:24 -0700

    more erro messaging fixes

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ else
 	fd = fopen(file, "r");
 	if (fd == NULL)
 		{
-		fprintf(stderr, "Error: Can't open file\n");
+		fprintf(stderr, "Error: Can't open file %s\n", file);
 		exit(EXIT_FAILURE);
 		}
 	for (li_numb = 1; (read_line) != -1; li_numb++)
